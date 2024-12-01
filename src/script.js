397ad62f6b7c33a54b1912dd4f5937b8d815a9c4
lynnec23 <lynnecarter23@gmail.com>
2024-12-02 00:13:33 +0800

    integration of weather icons and alteraltion of CSS to allow for img icons

--- src/script.js
+++ src/script.js
@@ -7,12 +7,16 @@ let humidityElement = document.querySelector("#humidity");
 let windSpeedElement = document.querySelector("#wind-speed");
 let timeElement = document.querySelector("#time");
 let date = new Date (response.data.time *1000);
+let iconElement = document.querySelector("#icon");
+
+
 
 cityElement.innerHTML = response.data.city;
 timeElement.innerHTML =  formatDate(date);
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+iconElement.innerHTML = `<img src ="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 temperatureElement.innerHTML = Math.round(temperature);
 }
 
